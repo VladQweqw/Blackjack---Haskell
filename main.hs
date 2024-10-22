6f64b0c9c66df7b0f36d53dadc0af241f5f9ba1f
Vlad <vladpoienariu@gmail.com>
2024-10-22 13:28:48 +0300

    merge tati, am facut si interfata sanki la misto ca bajetii

--- main.hs
+++ main.hs
@@ -1,11 +1,17 @@
 import System.Random (randomRIO)
 import Data.List (intercalate)
+import Text.Read (readMaybe)
 
-cards :: [String] = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+cards :: [String] = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J"]
 
 main :: IO ()
 main = do
-    putStrLn "=-=-=-=-=-= Blackjack =-=-=-=-=-=\n   >>>Press [ENTER] to start!"
+    putStrLn (
+        "*=-=-=-=-=-= Blackjack =-=-=-=-=-=*\n" ++ 
+        "|                                 |\n" ++ 
+        "|     Press [ENTER] to start!     |\n" ++ 
+        "|                                 |\n" ++ 
+        "*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*")
     input <- getLine -- Reads input
 
     if input == "" then
@@ -15,9 +21,6 @@ main = do
 
 getRNG :: Int -> IO Int
 getRNG range = randomRIO (1, range)
-
--- removeItem :: String -> [String] -> [String]
--- removeItem cardToDelete remainingCards = [x | x <- remainingCards, x /= cardToDelete]
 
 getCard :: IO String
 getCard = do
@@ -31,16 +34,83 @@ getInitialDeck = do
     secondCard <- getCard
     return [firstCard, secondCard]
 
-displayDecks :: [String] -> [String] -> IO ()
-displayDecks playerDeck robotDeck = do
-    putStr "User deck: "
+displayDecks :: [String] -> [String] -> Bool -> IO ()
+displayDecks playerDeck robotDeck showRobot = do
+    putStr ("+-----------------------+\n" ++
+           "| User deck: ")
     let playerConcat = foldr (\card acc -> card ++ " " ++ acc) "" playerDeck
-    putStr playerConcat
+    putStr playerConcat 
 
-    putStr "\nRobot deck: "
-    let robotConcat = foldr (\card acc -> card ++ " " ++ acc) "" robotDeck
-    putStr robotConcat
-    putStr "\n[ENTER] Hit | [SHIFT] Pass"
+    if showRobot then do
+        putStr "\n| Robot deck: "
+        let robotConcat = foldr (\card acc -> card ++ " " ++ acc) "" robotDeck
+        putStr robotConcat
+    else do
+        putStr "\n| Robot deck: ? "
+        let robotConcat = foldr (\card acc -> card ++ " " ++ acc) "" (tail robotDeck)
+        putStr robotConcat
+
+    putStr "\n+-----------------------+"
+    putStr "\n>>> [ENTER] Hit | [TYPE ANYTHING] Stay\n"
+
+strToInt :: String -> Maybe Int
+strToInt = readMaybe
+
+cardValueToINT :: String -> Int
+cardValueToINT card
+    | card == "J" || card == "Q" || card == "K" = 10
+    | card == "A" = 11
+    | otherwise = case strToInt card of
+                    Just number -> number
+
+calculateDeck :: [String] -> Int
+calculateDeck deck = sum (map cardValueToINT deck)
+
+isGameOver :: [String] -> [String] -> Bool -> String
+isGameOver playerDeck robotDeck isStay
+    | isStay && playerSum > robotSum = "Player wins"
+    | isStay && playerSum < robotSum = "Robot wins"
+    | isStay && playerSum == robotSum = "DRAW"
+    | playerSum >= 21 = "Robot wins"
+    | robotSum >= 21 = "Player wins"
+    | playerSum == 21 = "Player wins"
+    | robotSum == 21 = "Robot wins"
+    | otherwise = "Game continues"
+    where
+        playerSum = calculateDeck playerDeck
+        robotSum = calculateDeck robotDeck
+        
+
+fancyPrintGameStatus :: String -> IO()
+fancyPrintGameStatus str = do
+    putStrLn ("\n#=#=#=#=#=#=#=#=#=#=#=#=#\n >>> " ++ str ++ " <<< " ++ "\n#=#=#=#=#=#=#=#=#=#=#=#=#")
+
+whileLoop :: [String] -> [String] -> Bool -> IO()
+whileLoop playerCards robotCards gameOver
+    | gameOver = return ()
+    | otherwise = do
+            input <- getLine -- Reads input
+
+            if input == "" then do
+                playerNewCard <- getCard
+                let newPlayerCards = playerCards ++ [playerNewCard]
+
+                robotNewCard <- getCard
+                let newRobotCards = robotCards ++ [robotNewCard]
+
+                let gameStatus = isGameOver newPlayerCards newRobotCards False
+
+                
+                if gameStatus == "Game continues" then do
+                    displayDecks newPlayerCards newRobotCards False
+                    whileLoop newPlayerCards newRobotCards False
+                else do
+                    displayDecks newPlayerCards newRobotCards True
+                    fancyPrintGameStatus gameStatus
+            else do
+                displayDecks playerCards robotCards True
+                fancyPrintGameStatus (isGameOver playerCards robotCards True)
+                
 
 
 startGame :: IO ()
@@ -48,5 +118,9 @@ startGame = do
     playerCards <- getInitialDeck
     robotCards <- getInitialDeck
     
-    displayDecks playerCards robotCards
+    displayDecks playerCards robotCards False
+    whileLoop playerCards robotCards False
+
+
+
 
